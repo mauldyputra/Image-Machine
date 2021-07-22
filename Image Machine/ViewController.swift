c5117605e84ce5701fa47c64d617c647b7b07984
mauldyputra <49427045+mauldyputra@users.noreply.github.com>
2021-07-22 09:14:29 +0700

    Add files via upload
    Project Init

---
+++ Image Machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Machine
+//
+//  Created by Mauldy Putra on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
