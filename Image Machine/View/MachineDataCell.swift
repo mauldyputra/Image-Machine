3bea589f602315b2f550298d03751c26d2aad6dd
Mauldy Putra Pratama <mauldyputra@gmail.com>
2021-07-22 13:53:31 +0700

    Init Machine Data Cell

---
+++ Image Machine/View/MachineDataCell.swift
@@ -0,0 +1,23 @@
+//
+//  MachineDataCell.swift
+//  Image Machine
+//
+//  Created by Mauldy Putra on 22/07/21.
+//
+
+import UIKit
+
+class MachineDataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
